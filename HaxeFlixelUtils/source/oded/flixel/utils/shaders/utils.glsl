1dcc133df8dcd7cd9151a67e5375641a044ef6b6
Oded Rosenblatt <odedro987@gmail.com>
2022-07-17 00:39:14 +0200

    Added util funcs

---
+++ HaxeFlixelUtils/source/oded/flixel/utils/shaders/utils.glsl
@@ -0,0 +1,24 @@
+vec2 offsetToCenter(vec2 uv){
+    float xSign = openfl_TextureCoord.x == 0. ? 1. : -1.;
+    float ySign = openfl_TextureCoord.y == 0. ? 1. : -1.;
+
+    float xOffset = (openfl_TextureSize.x / 2.) * xSign;
+    float yOffset = (openfl_TextureSize.y / 2.) * ySign;
+
+    return vec2(uv.x + xOffset, uv.y + yOffset);
+}
+
+mat2 rotateDeg(float angle){
+    angle *= (3.14 / 180.);
+    return mat2(cos(angle),-sin(angle),
+                sin(angle),cos(angle));
+}
+
+mat2 rotateRad(float rad){
+    return mat2(cos(rad),-sin(rad),
+                sin(rad),cos(rad));
+}
+
+float sine(float x, float offset, float amplitude, float frequency){
+    return amplitude * sin((x + offset) * frequency);
+}
